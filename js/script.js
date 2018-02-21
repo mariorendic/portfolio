dd8c20599d1f8dc590f8f84ed0d944a605c10005
Mario <mario@simplesolutions.hr>
2018-02-21 14:06:44 +0100

    1

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(e) {
+
+    /* browser height */
+    $(window).bind("load resize", function(e) {
+
+        $('.hero').css("height", $(window).height() * 1);
+
+    });
+
+
+    $("#nav-toggle, .overlay, nav a").click(function(e) {
+        e.preventDefault();
+        $("nav").toggleClass("nav-open");
+        $(".overlay").toggleClass("overlay-on");
+    });
+ 
+
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: (target.offset().top)
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+
+
+    });
+
+});
