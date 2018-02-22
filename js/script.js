c8db0cd50be927d24afd59e342554bec13c713a5
Mario <mario@simplesolutions.hr>
2018-02-22 02:01:30 +0100

    beta 3

--- js/script.js
+++ js/script.js
@@ -1,46 +1,49 @@
-$(document).ready(function(e) {
+  /* browser height */
+  $(window).bind("load resize ready", function(e) {
 
-    /* browser height */
-    $(window).bind("load resize ready", function() {
+      $('.hero, .browser-height').css("min-height", $(window).height() * 1);
 
-        $('.hero, .browser-height').css("min-height", $(window).height() * 1);
-
-    });
+  });
 
 
-    $("#nav-toggle, .overlay, nav a").click(function() {
-        e.preventDefault();
-        $("nav").toggleClass("nav-open");
-        $(".overlay").toggleClass("overlay-on");
-    });
+  $(document).ready(function(e) {
 
 
-    $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: (target.offset().top)
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
 
-    });
+      $("#nav-toggle, .overlay, nav a").click(function(e) {
+          e.preventDefault();
+          $("nav").toggleClass("nav-open");
+          $(".overlay").toggleClass("overlay-on");
+      });
 
-    $(window).bind("load resize scroll", function() {
 
-        if ($(window).scrollTop() > 130) {
-            $('header').addClass('header-scroll');
+      $(function() {
+          $('a[href*="#"]:not([href="#"])').click(function() {
+              if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                  var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
-        } else {
-            $('header').removeClass('header-scroll');
+                  if (target.length) {
+                      $('html, body').animate({
+                          scrollTop: (target.offset().top)
+                      }, 1000);
+                      return false;
+                  }
+              }
+          });
 
-        }
-    });
+      });
 
-});
+      $(window).bind("load resize scroll", function(e) {
+
+          if ($(window).scrollTop() > 130) {
+              $('header').addClass('header-scroll');
+
+          } else {
+              $('header').removeClass('header-scroll');
+
+          }
+      });
+
+  });
